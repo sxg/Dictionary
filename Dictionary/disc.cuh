4b9a268123523bdad1dec6066a71101957150812
Satyam Ghodasara <sghodas@gmail.com>
2018-02-07 21:20:48 -0500

    Created disc header file

---
+++ Dictionary/disc.cuh
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+void disc(double *contrast, const double *times, const double *artConc, const double *pvConc, const double af, const double dv, const double mtt, const double tauA, const double tauP);
