72f20254e4fbcbc12fe9a5f36e9ff5409528f57a
Satyam Ghodasara <sghodas@gmail.com>
2018-02-07 23:31:59 -0500

    Added signatures for signal to contrast functions

--- Dictionary/disc.h
+++ Dictionary/disc.h
@@ -1,3 +1,8 @@
 
 // Dual-input single-compartment model
 double* disc(const double *times, const double *artConc, const double *pvConc, const double af, const double dv, const double mtt, const double tauA, const double tauP);
+
+// Signal to contrast conversion
+double* artSignal2contrast(const double *artSignal, const double *pvSignal, const double flipAngle, const double t1Blood, const double tr, const double t1Contrast, const int startFrame, const int addFrames);
+double* pvSignal2contrast(const double *pvSignal, const double flipAngle, const double t1Plasma, const double tr, const double t1Contrast, const int startFrame, const int addFrames);
+double* signal2contrast(const double *signal, const double flipAngle, const double t1Liver, const double tr, const double t1Contrast, const int startFrame, const int addFrames, const double scaleFactor);
