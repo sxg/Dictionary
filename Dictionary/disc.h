a095534d96de4a234e9ec80aa87c9811f12f0d1c
Satyam Ghodasara <sghodas@gmail.com>
2018-02-07 22:49:08 -0500

    Removed CUDA implementation of the disc model

---
+++ Dictionary/disc.h
@@ -0,0 +1,3 @@
+
+// Dual-input single-compartment model
+double* disc(const double *times, const double *artConc, const double *pvConc, const double af, const double dv, const double mtt, const double tauA, const double tauP);
