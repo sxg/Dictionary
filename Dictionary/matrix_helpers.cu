696568c78f7c80248e0526628682b55564a0b27c
Satyam Ghodasara <sghodas@gmail.com>
2018-02-11 23:37:06 -0500

    Added row <-> col major matrix converters

--- Dictionary/matrix_helpers.cu
+++ Dictionary/matrix_helpers.cu
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 int getRowMajorIndex(const int colMajorIndex, const int width, const int height) {
 	int row = colMajorIndex % height;
 	int col = colMajorIndex / height;
@@ -6,4 +8,26 @@ int getRowMajorIndex(const int colMajorIndex, const int width, const int height)
 
 int getColMajorIndex(const int rowMajorIndex, const int width, const int height) {
 	return getRowMajorIndex(rowMajorIndex, height, width);
+}
+
+double *getRowMajorMatrix(const double *colMajorMatrix, const int width, const int height) {
+	int colMajorIndex, rowMajorIndex;
+	double *rowMajorMatrix = (double *)malloc(sizeof(double) * width * height);
+
+	for (colMajorIndex = 0; colMajorIndex < width * height: colMajorIndex++) {
+		rowMajorIndex = getRowMajorIndex(colMajorIndex, width, height);
+		rowMajorMatrix[rowMajorIndex] = colMajorMatrix[colMajorIndex];
+	}
+	return rowMajorMatrix;
+}
+
+double *getColMajorMatrix(const double *rowMajorMatrix, const int width, const int height) {
+	int colMajorIndex, rowMajorIndex;
+	double *colMajorMatrix = (double *)malloc(sizeof(double) * width * height);
+
+	for (rowMajorIndex = 0; rowMajorIndex < width * height; rowMajorIndex++) {
+		colMajorIndex = getColMajorIndex(rowMajorIndex, width, height);
+		colMajorMatrix[colMajorIndex] = rowMajorMatrix[rowMajorIndex];
+	}
+	return colMajorMatrix;
 }
--- Dictionary/matrix_helpers.h
+++ Dictionary/matrix_helpers.h
@@ -1,3 +1,6 @@
 // Row <-> col major interconverters
 int getRowMajorIndex(const int colMajorIndex, const int width, const int height);
 int getColMajorIndex(const int rowMajorIndex, const int width, const int height);
+
+double *getRowMajorMatrix(const double *colMajorMatrix, const int width, const int height);
+double *getColMajorMatrix(const double *rowMajorMatrix, const int width, const int height);
